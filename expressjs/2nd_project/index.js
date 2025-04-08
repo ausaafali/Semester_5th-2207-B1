63812e6b25d951fa81cb5a0b65cfa130f6051cd1
hanifa-patel14 <117252574+hanifa-patel14@users.noreply.github.com>
2025-04-08 11:09:26 +0500

    firsto

--- expressjs/2nd_project/index.js
+++ expressjs/2nd_project/index.js
@@ -1,8 +1,8 @@
 let exp = require('express');
 let route = require('./routes/router');
 let healthInfo = require('./collections/healthinfo');
-let personalRecord = require('./collections/personal');
-let user = require('./collections/user');
+let personalRecord = require('./collections/personal')
+let user = require('./collections/user')
 require('dotenv').config();
 let db = require('./connect_to_db/connection')
 
@@ -17,18 +17,6 @@ let add_user = async function () {
             userAge: 20,
             userAddress: 'street 14/45',
         })
-        personalRecord.create({
-            height: 180,
-            weight: 70,
-            BMI: 35,
-            category: 'genda',
-        })
-        healthInfo.create({
-            sugar: 100,
-            heartrate: 90,
-            BP: 140,
-            colestrol: 170
-        })
         console.log("Data Added successfully")
     } catch (error) {
         console.error(error);
@@ -38,12 +26,12 @@ let add_user = async function () {
 // add personal record data function
 let add_personal = async function () {
     try {
-        // personalRecord.create({
-        //     height: 180,
-        //     weight: 70,
-        //     BMI: 35,
-        //     category: 'genda',
-        // })
+        personalRecord.create({
+            height: 180,
+            weight: 70,
+            BMI: 35,
+            category: 'genda',
+        })
         console.log("Personal Bio added successfully")
     } catch (error) {
         console.error(error);
@@ -52,12 +40,12 @@ let add_personal = async function () {
 // add healthInfo data function
 let add_health = async function () {
     try {
-        // healthInfo.create({
-        //     sugar: 100,
-        //     heartrate: 90,
-        //     BP: 140,
-        //     colestrol: 170
-        // })
+        healthInfo.create({
+            sugar: 100,
+            heartrate: 90,
+            BP: 140,
+            colestrol: 170
+        })
         console.log("Health recorded successfully")
     } catch (error) {
         console.error(error);
@@ -84,9 +72,9 @@ let app = exp();
 // app.use('/user', user)
 
 db().then(() => {
-    // add_health()
-    // add_personal()
-    add_user()
+    add_personal();
+    add_user();
+    add_health();
     app.listen(process.env.PORT, () => {
         console.log(`server is running on port http://localhost:${process.env.PORT}/ikrama/route/`);
     })
